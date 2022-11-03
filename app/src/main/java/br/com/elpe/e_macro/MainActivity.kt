6debe86f23a0a4645e49b07e76fee9f968468cc1
Eliabe Santos <eliabesantos1106@gmail.com>
2022-11-03 12:23:29 -0300

    main_view

--- app/src/main/java/br/com/elpe/e_macro/MainActivity.kt
+++ app/src/main/java/br/com/elpe/e_macro/MainActivity.kt
@@ -5,17 +5,13 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import br.com.elpe.e_macro.databinding.ActivityMainBinding
+//import br.com.elpe.e_macro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.rollButton.setOnClickListener{ rollDice()};
-    //setContentView(R.layout.activity_main)
-        //val rollButton: Button = findViewById(R.id.roll_button)
-        //rollButton.setOnClickListener { rollDice() }
+        setContentView(R.layout.activity_main)
     }
     private fun rollDice() {
         Toast.makeText(this, "button clicked",
